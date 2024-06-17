87859d2bac431b9cbf2553fb963cebb767108516
Robert A Dixon <robmsc24@gmail.com>
2024-06-17 05:18:49 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import { Home } from './_root/pages';
 import './globals.css';
-import AuthLayout from './_auth/forms/AuthLayout';
+import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/pages/RootLayout';
 
 const App = () => {
