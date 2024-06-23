7f00dc3d02c4d94cc5a7c2a2e8dc03743514b274
Robert A Dixon <robmsc24@gmail.com>
2024-06-23 09:13:59 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/pages/RootLayout';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
