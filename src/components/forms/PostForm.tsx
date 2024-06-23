102321ff086e4f2a34ba08ed3021493b0b1f438e
Robert A Dixon <robmsc24@gmail.com>
2024-06-23 13:03:31 -0400

    wip

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -13,6 +13,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+import FileUploader from "../shared/FileUploader"
 
  
 const formSchema = z.object({
@@ -39,20 +41,30 @@ const PostForm = () => {
 
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
       <FormField
         control={form.control}
-        name="username"
+        name="caption"
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Username</FormLabel>
+            <FormLabel className="shad-form_label">Caption</FormLabel>
             <FormControl>
-              <Input placeholder="shadcn" {...field} />
+              <Textarea className="shad-textarea custom-scrollbar" {...field} />
             </FormControl>
-            <FormDescription>
-              This is your public display name.
-            </FormDescription>
-            <FormMessage />
+            <FormMessage className="shad-form_message"/>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="file"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="shad-form_label">Add Photos</FormLabel>
+            <FormControl>
+              <FileUploader />
+            </FormControl>
+            <FormMessage className="shad-form_message"/>
           </FormItem>
         )}
       />
