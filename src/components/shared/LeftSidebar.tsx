a18f9cddc7ba8ff65122a5929bc4fcf371ce1e8b
Robert A Dixon <robmsc24@gmail.com>
2024-06-23 11:27:17 -0400

    wip

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -19,7 +19,7 @@ const LeftSidebar = () => {
   return (
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
-      <Link to="/" className="flex gap-3 items-center">
+        <Link to="/" className="flex gap-3 items-center">
           <img
             src="/assets/images/logo.svg"
             alt="logo"
@@ -29,19 +29,19 @@ const LeftSidebar = () => {
         </Link>
 
         <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
-        <img 
-          src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
-          alt="profile"
-          className="h-14 w-14 rounded-full"
-        />
-        <div className="flex flex-col">
-          <p className="body-bold">
-            {user.name}
-          </p>
-          <p className="small-regular text-light-3">
-            @{user.username}
-          </p>
-        </div>
+          <img 
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">
+              {user.name}
+            </p>
+            <p className="small-regular text-light-3">
+              @{user.username}
+            </p>
+          </div>
         </Link>
 
         <ul className="flex flex-col gap-6">
@@ -49,9 +49,7 @@ const LeftSidebar = () => {
             const isActive = pathname === link.route;
 
             return (
-              <li key={link.label} className={`leftsidebar-link ${
-                isActive ? 'bg-primary-500' : ''
-                }`}>
+              <li key={link.label} className={`leftsidebar-link ${isActive ? 'bg-primary-500' : ''}`}>
                 <NavLink
                   to={link.route}
                   className="flex gap-4 items-center p-4 group"
@@ -59,23 +57,24 @@ const LeftSidebar = () => {
                   <img 
                     src={link.imgURL}
                     alt={link.label}
-                    className={`transition-all duration-200 ${isActive ? 'filter invert' : 'group-hover:filter group-hover:invert'}`}
+                    className={`transition-all duration-200 ${isActive ? 'filter invert-white' : 'group-hover:filter group-hover:invert-white'}`}
                   />
                   {link.label}
                 </NavLink>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
 
       <Button 
-      variant="ghost" 
-      className="shad-button_ghost" 
-      onClick={() => signOut()}>
+        variant="ghost" 
+        className="shad-button_ghost" 
+        onClick={() => signOut()}
+      >
         <img src="/assets/icons/logout.svg" alt="logout" />
         <p className="small-medium lg:base-medium">Logout</p>
-          </Button>
+      </Button>
     </nav>
   );
 }
