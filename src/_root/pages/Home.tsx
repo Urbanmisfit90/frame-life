a16ce31290d68b33c7f120478f8f69d3890d6e7c
Robert A Dixon <robmsc24@gmail.com>
2024-06-26 00:25:41 -0400

    wip

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Loader from "@/components/shared/Loader";
 import { useGetRecentPost } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Home = () => {
 const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetRecentPost()
@@ -12,8 +13,10 @@ const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetR
         {isPostLoading && !posts ? (
           <Loader />
         ) : (
-          <ul>
-
+          <ul className="flex flex-col flex-1 gap-9 w-full">
+            {posts?.documents.map((post: Models.Document) => (
+              <li>{post.title} </li>
+            ))}
           </ul>
         )}
         </div>
