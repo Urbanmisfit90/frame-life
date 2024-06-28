0189f56ee68ee47961c2c681a885ab9d22dd1c72
Robert A Dixon <robmsc24@gmail.com>
2024-06-27 22:05:17 -0400

    wip

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -10,15 +10,22 @@ const PostCard = ({ post }: PostCardProps) => {
     <div className="post-card">
       <div className="flex-between">
         <div className="flex items-center gap">
-          <Link to={`/profile/${post.creator.$id}`}>
-            <img
-              src={post?.creator?.imageUrl || "/assets/icons/profile.svg"}
-              alt="creator"
-              className="rounded-full w-12 lg:h-12"
-            />
-          </Link>
+          {post.creator && (
+            <Link to={`/profile/${post.creator.$id}`}>
+              <img
+                src={post?.creator?.imageUrl || "/assets/icons/profile.svg"}
+                alt="creator"
+                className="rounded-full w-12 lg:h-12"
+              />
+            </Link>
+          )}
 
-          <div>
+          <div className="flex flex-col ml-2"> {/* Added margin-left for spacing */}
+            {post.creator && (
+              <p className="mt-1"> {/* Added margin-top for spacing */}
+                {post.creator.name}
+              </p>
+            )}
           </div>
         </div>
       </div>
