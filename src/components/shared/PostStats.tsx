09927523ca244a1fe15c003a6ae0d8472f62b122
Robert A Dixon <robmsc24@gmail.com>
2024-06-28 16:56:52 -0400

    wip

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -29,22 +29,25 @@ const PostStats = ({ post, userId } : PostStatsProps) => {
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
         <img 
-        src={`${checkIsLiked(likes,userId) 
+        src={checkIsLiked(likes,userId) 
           ? "/assets/icons/liked.svg"
-          : "/assets/icons/like.svg"}
-          `}
+          : "/assets/icons/like.svg"
+        }
         alt="like"
         width={20}
         height={20}
-        onClick={() => {}}
+        onClick={handleLikePost}
         className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">0</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
 
       <div className="flex gap-2">
         <img 
-        src="/assets/icons/save.svg"
+        src={isSaved
+        ? "/assets/icons/saved.svg"
+        : "/assets/icons/save.svg"
+        }
         alt="like"
         width={20}
         height={20}
