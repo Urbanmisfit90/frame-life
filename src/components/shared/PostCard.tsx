6af96fab3fca93518317a32f02259eb9e09aa962
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-06-27 20:10:37 -0400

    updated some fixes

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,31 +1,35 @@
 import { Models } from "appwrite";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 type PostCardProps = {
-  post: Models.Document;  
-}
+  post: Models.Document;
+};
 
 const PostCard = ({ post }: PostCardProps) => {
+  console.log(post);
   return (
     <div className="post-card">
-        <div className="flex-between">
-          <div className="flex items-center gap">
-            <Link to={`/profile/${post.creator.$id}`}>   
-            <img 
-            src={post?.creator?.imageUrl || '/assets/icons/profile.svg'}
-            alt='creator'
-            className="rounded-full w-12 lg:h-12"
-            />         
-            </Link>
+      <div className="flex-between">
+        <div className="flex items-center gap">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={post?.creator?.imageUrl || "/assets/icons/profile.svg"}
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
 
-            <div>
-            
-            </div>
+          <div>
+            <img
+              src={post?.imageUrl || "/assets/icons/profile.svg"}
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
           </div>
-
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
