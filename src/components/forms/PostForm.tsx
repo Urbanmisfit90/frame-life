78722910f4a0fe02667138449e12ba6c58cb35df
Robert A Dixon <robmsc24@gmail.com>
2024-07-05 15:31:52 -0400

    wip

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -52,8 +52,6 @@ const PostForm = ({ post, action }: PostFormProps) => {
       navigate('/')
     }
 
-    console.log(post?.imageUrl)
-
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
