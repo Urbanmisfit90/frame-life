755ce8c3c8339a500152dc1338d3f6cab549640a
Robert A Dixon <robmsc24@gmail.com>
2024-07-09 16:24:54 -0400

    wip

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,7 +1,7 @@
+import { useParams } from "react-router-dom"
 import PostForm from "@/components/forms/PostForm"
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { Loader } from "lucide-react";
-import { useParams } from "react-router-dom"
 
 const EditPost = () => {
   const { id } = useParams();
@@ -22,7 +22,7 @@ const EditPost = () => {
           <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
         </div>
 
-        <PostForm action="update" post={post} />
+        <PostForm action="Update" post={post} />
       </div>
     </div>
   )
