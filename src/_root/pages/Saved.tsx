237031c06b7def4ddb66895368862208f8346e43
Robert A Dixon <robmsc24@gmail.com>
2024-07-11 21:00:59 -0400

    wip

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,34 +1,38 @@
-import { useGetSavePosts } from '@/lib/react-query/queriesAndMutations'
-import React from 'react'
+import React from 'react';
+import Loader from '@/components/shared/Loader';
+import PostCard from '@/components/shared/PostCard';
+import { useGetSavePosts } from '@/lib/react-query/queriesAndMutations';
+import { Models } from 'appwrite';
 
 const Saved = () => {
-  const { data: posts, isLoading, error } = useGetSavePosts()
+  const { data: savedPosts, isLoading, error } = useGetSavePosts();
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <Loader />;
   }
 
   if (error) {
-    return <div>Error loading saved posts</div>
+    return <div>Error loading saved posts</div>;
   }
 
   return (
-    <div>
-      <h1>Saved Posts</h1>
-      {posts && posts.documents.length > 0 ? (
-        posts.documents.map((post) => (
-          <div key={post.$id}>
-            <h2>{post.caption}</h2>
-            <img src={post.imageUrl} alt={post.caption} />
-            <p>{post.location}</p>
-            <p>{post.tags.join(', ')}</p>
-          </div>
-        ))
-      ) : (
-        <div>No saved posts found.</div>
-      )}
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h1 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h1>
+          {savedPosts && savedPosts.documents.length > 0 ? (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {savedPosts.documents.map((post: Models.Document) => (
+                <PostCard key={post.$id} post={post} />
+              ))}
+            </ul>
+          ) : (
+            <div>No saved posts found.</div>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Saved
+export default Saved;
