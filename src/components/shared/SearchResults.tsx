72c614361806ee1c71ba9bc7d2c0c93ffc740f40
Robert A Dixon <robmsc24@gmail.com>
2024-07-13 19:17:55 -0400

    wip

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -10,15 +10,15 @@ type SearchResultsProps = {
 const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
   if(isSearchFetching) return <Loader />
 
-  if(searchedPosts && searchedPosts.documents.length > 0) {
-    return (
-  <GridPostList posts={searchedPosts.documents} />
-  )
-    }
-
   return (
-    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
-  )
-}
+    <>
+      {searchedPosts.length > 0 ? (
+        <GridPostList posts={searchedPosts} />
+      ) : (
+        <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+      )}
+    </>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
